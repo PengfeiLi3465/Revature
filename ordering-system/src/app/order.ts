234ed45a6ad88eaa496da4758157ef5bd3238fe4
Pengfei Li <pengfeili3465@gmail.com>
2021-11-09 22:18:49 -0500

    p2 backend added

---
+++ ordering-system/src/app/order.ts
@@ -0,0 +1,7 @@
+export interface Order{
+    orderId: number;
+    userId: number;
+    conditiontypeId: number;
+    totalPrice: number;
+    orderNote: String;   
+}
---
+++ ordering-system/src/app/user.ts
@@ -0,0 +1,7 @@
+export interface User{
+    userId:number;
+    usertypeId:number;
+    userEmail:string;
+    userName:string;
+    userPassword:string;
+}
